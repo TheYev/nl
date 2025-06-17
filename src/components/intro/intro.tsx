8684deac28f24ce9540f68ced15c4cfe9aa4efb1
serveyk0-work <serveyko.work@gmail.com>
2025-06-18 00:05:01 +0300

    add layout, fix first screen without mobile

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -1,36 +1,29 @@
 import Image from "next/image"
 import styles from "./styles/intro.module.css"
-import { Urls } from "@/utils/Urls"
+import {Urls} from "@/utils/Urls"
 
 export const Intro = () => {
-    return (
-        <div className={styles.intro}>
-            <Image
-                src="block_1.svg"
-                alt="Woman"
-                width={580}
-                height={580}
-                className={styles.image}
-            />
+  return (
+    <div className={styles.intro}>
+      <Image
+        src="block_1.svg"
+        alt="Woman"
+        className={styles.image}
+        width={510}
+        height={510}
+      />
 
-            <div className={styles.info_block}>
-                <h1 className={styles.head}>Turn More Chats into Cash — AI Messaging for OnlyFans& Fansly</h1>
-                <p className={styles.info}>Trusted by top 1% creators to boost revenue and save time. Our AI assistant keeps fans engaged, increases tips, and drives sales — all through seamless Chrome extension integration.</p>
-                <div className={styles.bttn_info}>
-
-                    <Image
-                        src="block_1.svg"
-                        alt="Woman"
-                        width={280}
-                        height={280}
-                        className={styles.imageMobile}
-                    />
-                    <a href={Urls.GET_EARLY_ACCESS}>
-                        <button className={styles.bttn}>Get Early Access</button>
-                    </a>
-                    <p className={styles.bttn_description}>Beta access is limited — reserve your spot now.</p>
-                </div>
-            </div>
+      <div className={styles.info_block}>
+        <h1 className={styles.head}>Turn More Chats into Cash — AI Messaging for OnlyFans & Fansly</h1>
+        <p className={styles.info}>Trusted by top 1% creators to boost revenue and save time. Our AI assistant keeps
+          fans engaged, increases tips, and drives sales — all through seamless Chrome extension integration.</p>
+        <div className={styles.bttn_info}>
+          <a href={Urls.GET_EARLY_ACCESS}>
+            <button className={styles.bttn}>Get Early Access</button>
+          </a>
+          <p className={styles.bttn_description}>Beta access is limited — reserve your spot now.</p>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
